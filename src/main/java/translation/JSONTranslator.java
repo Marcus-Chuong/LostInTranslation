7c9762859b009546be55a18bc656241b0f0a96fc
Marcus Chuong <marcus.chuong@mail.utoronto.ca>
2025-09-22 16:08:08 -0400

    finished taskc

--- src/main/java/translation/JSONTranslator.java
+++ src/main/java/translation/JSONTranslator.java
@@ -49,8 +49,6 @@ public class JSONTranslator implements Translator {
                 JSONObject countryData = jsonArray.getJSONObject(i);
                 String countryCode = countryData.getString("alpha3");
 
-                List<String> languages = new ArrayList<>();
-
                 countryCodes.add(countryCode);
 
                 // iterate through the other keys to get the information that we need
@@ -60,8 +58,8 @@ public class JSONTranslator implements Translator {
 
                         translations.put(languageCode + "-" + countryCode,  countryData.getString(key));
 
-                        if (!languages.contains(languageCode)) {
-                            languages.add(languageCode);
+                        if (!languageCodes.contains(languageCode)) {
+                            languageCodes.add(languageCode);
                         }
                     }
                 }
